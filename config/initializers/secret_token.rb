b3068cd41921a7f86929608c7469d4fb37a6693c
Jon Moses <jon@burningbush.us>
2011-10-19 20:54:19 -0400

    Base rails3 app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+KegeratorstatUs::Application.config.secret_token = '2ee0044377daff42039ff6460d6c8d47efceb751d162275aac342685e7c96d453ca6ea103b4726f4697724f52a513043ce2442b6ec685b81d655aeba16596bb1'
