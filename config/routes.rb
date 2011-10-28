dd9ae2ebc8d1c464f0e27b132d92e18c4017bc3e
Jon Moses <jon@burningbush.us>
2011-10-27 21:17:49 -0400

    Routes?

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ KegeratorstatUs::Application.routes.draw do
     resources :kegs
   end
 
-  match 'status/:login' => 'status#for_user', :as => :status
+  match 'status/user/:login' => 'status#for_user', :as => :status
 
   root :to => 'status#index'
 end
