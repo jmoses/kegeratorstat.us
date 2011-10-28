89c2ee0ecf9bc1cf3160df55c7504ee732de0de3
Jon Moses <jon@burningbush.us>
2011-10-27 21:08:19 -0400

    Private is for pussies

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,5 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
-  before_filter :http_auth if Rails.env.production?
-
   protected
-    def http_auth
-      authenticate_or_request_with_http_digest("Application") do |name|
-        {
-          "jmoses" => "sesom3"
-        }[name]
-      end
-    end
 end
