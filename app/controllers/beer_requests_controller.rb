30f0cb96abe5b8bb3c607e79299a89aaa12f71c4
Jon Moses <jon@burningbush.us>
2011-10-27 23:14:08 -0400

    Don't blow up

--- app/controllers/beer_requests_controller.rb
+++ app/controllers/beer_requests_controller.rb
@@ -7,7 +7,7 @@ class BeerRequestsController < ApplicationController
     create! do |format|
       format.html do
         flash[:success] = "Request submitted!"
-        Mailer.beer_request(@beer_request).deliver
+        Mailer.beer_request(@beer_request).deliver rescue nil
         redirect_to kegerators_path
       end
     end
