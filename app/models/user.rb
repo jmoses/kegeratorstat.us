45ed4b0e241313b14fcee13683af216645c613bb
Jon Moses <jon@burningbush.us>
2011-10-27 21:25:49 -0400

    Statu page

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   validates_uniqueness_of :login
+  validates_presence_of :login
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me, :login
